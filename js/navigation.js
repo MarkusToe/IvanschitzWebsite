38bba64faf28f594bff389d277cfe55d7232527b
Markus Török <markus.toeroek@gmail.com>
2015-03-20 11:47:29 +0100

    initial commit

---
+++ js/navigation.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+    $('.control').click(function(e){
+        e.preventDefault();
+        console.log('clicked');
+        $('body').toggleClass('active-nav');
+    })
+});
